a5856930da7aa98f93f4f8f2252770c55e3f0ea8
Vũ Tiến Mừng <mungvt@datascience.com.vn>
2021-03-24 10:42:31 +0700

    updata

--- bq_code.py
+++ bq_code.py
@@ -1,33 +1,43 @@
-# from google.cloud import bigquery
+from google.cloud import bigquery
+from google.api_core import exceptions
 
-# Construct a BigQuery client object.
-# client = bigquery.Client()
-#
-# # TODO(developer): Set table_id to the ID of the table to create.
-# table_id = "my-firebase-project-0001.btc_auto.test"
-#
-# schema = [
-#     bigquery.SchemaField("full_name", "STRING", mode="REQUIRED"),
-#     bigquery.SchemaField("age", "INTEGER", mode="REQUIRED"),
-# ]
-#
-# table = bigquery.Table(table_id, schema=schema)
-# table = client.create_table(table)  # Make an API request.
-# print(
-#     "Created table {}.{}.{}".format(table.project, table.dataset_id, table.table_id)
-# )
-#
-#
-# def get_table_ref(dataset_id, table_id):
-#     """
-#     Get table ref to project and dataset
-#     :param dataset_id:
-#     :param table_id:
-#     :return:
-#     """
-#     dataset_ref = bigquery.DatasetReference(project, dataset_id)
-#     table_ref = bigquery.TableReference(dataset_ref, table_id)
-#     return table_ref
 
-from GoogleCloudBigQuery import GoogleCloudBigQuery
-gcbq = GoogleCloudBigQuery('my-firebase-project-0001')
+project_id = 'my-firebase-project-0001'
+bq_client = bigquery.Client(project=project_id)
+
+
+def bq_create_partition_table(
+        dataset_id,
+        tbl_id,
+        schema,
+        part_type,
+        part_field
+):
+    tbl_ref = table_ref(dataset_id, tbl_id)
+
+    try:
+        bq_client.get_table(tbl_ref)
+    except exceptions.NotFound:
+        table = bigquery.Table(tbl_ref, schema=schema)
+        table.time_partitioning = bigquery.TimePartitioning(
+            type_=part_type,
+            field=part_field,
+            expiration_ms=600000
+        )
+        bq_client.create_table(table)
+
+
+def table_ref(dataset_id, tbl_id):
+    dataset_ref = bigquery.DatasetReference(dataset_id=dataset_id, project=project_id)
+    tbl_ref = dataset_ref.table(tbl_id)
+    return tbl_ref
+
+
+if __name__ == '__main__':
+    schema = [
+        bigquery.SchemaField('unix', 'TIMESTAMP', mode='REQUIRED'),
+        bigquery.SchemaField('name', 'STRING', mode='REQUIRED'),
+        bigquery.SchemaField('age', 'INTEGER', mode='REQUIRED'),
+    ]
+
+    bq_create_partition_table('btc_auto', 'test2', schema, 'HOUR', 'unix')
