1bd22d782d0b44abba6c7881a8b60ce6f39d7ba5
mungvt <vutienmung@gmail.com>
2021-04-27 16:44:09 +0700

    add luigi wf

---
+++ luigi_wf/luigi_wf.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+import luigi
+from logic import logic, crawl, bq_code
+
+
+class BaseTask(luigi.Task):
+    """
+    Output Create a Abstract Class
+    """
+    date = luigi.DateParameter(default=datetime.now())
+
+    def output(self):
+        return logic.create_target(self)
+
+    def execute(self):
+        pass
+
+    def run(self):
+        cls_name = self.__class__.__name__
+        # logic.write_log("Start {}. [{}]".format(cls_name, self.date))
+        print("Start {}. [{}]".format(cls_name, self.date))
+        try:
+            self.execute()
+        except Exception as e:
+            # logic.write_log("Exception.[{}]".format(str(e)))
+            print("Exception.[{}]".format(str(e)))
+            raise
+        logic.touch_target(self, cls_name)
+        # logic.write_log("End {}.".format(cls_name))
+        print("End {}.".format(cls_name))
+        return
+
+
+class CrawlData(BaseTask):
+    def execute(self):
+        crawl.crawl_data()
+
+
+class CreateAllTables(BaseTask):
+    def requires(self):
+        return CrawlData()
+
+    def execute(self):
+        bq_code.create_all_tables()
+
+
+class ImportData(BaseTask):
+    def requires(self):
+        return CrawlData()
+
+    def execute(self):
+        bq_code.load_btc_data_into_bq()
+
+
+class AllTasks(luigi.WrapperTask):
+    """
+    Output A WrapperTask
+    """
+    def requires(self):
+        return ImportData()
